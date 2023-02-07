9c2add17dfeeb4006a2be927563edf0a888cfc43
AdlerEngr <121722931+adlerengr@users.noreply.github.com>
2023-02-07 13:11:34 +0100

    Add files via upload

---
+++ 20230207.STEST.LS.002.CANS.py
@@ -0,0 +1,22 @@
+import machine
+from time import sleep_ms
+from uPy_APDS9900.apds9900LITE import APDS9900LITE
+from machine import Pin
+
+#LSens=Pin(20,Pin.OUT)
+#LSens.value(1);
+
+#Init I2C Buss on RP2040
+i2c =  machine.I2C(0,scl=machine.Pin(17), sda=machine.Pin(16))
+
+apds9900=APDS9900LITE(i2c)      # Enable sensor
+apds9900.prox.enableSensor()    # Enable Proximit sensing
+
+while True:
+        sleep_ms(25) # wait for readout to be ready
+        #print(apds9900.prox.proximityLevel)   #Print the proximity value
+        if apds9900.prox.proximityLevel > 1000:
+            print('Turn')
+        else:
+            print('Run')
+    
